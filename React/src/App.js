b505874f598e1193ab2a2692c8d3e3b41ca40f91
Kevin Jolley <serrowpls@gmail.com>
2018-12-01 18:05:11 -0600

    react caught-up

--- React/src/App.js
+++ React/src/App.js
@@ -3,8 +3,41 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Henlo (OvO")</h1>
+      <div className="main_container">
+        {/* Navigation */}
+        <div className="navigation" id="nav">
+          <div className="nav_head">
+            <a href="#top">
+              {' '}
+              <p>K/J</p>{' '}
+            </a>
+          </div>
+          <div className="nav_items">
+            <p>Github</p>
+            <p>LinkedIn</p>
+            <p>Resume</p>
+          </div>
+        </div>
+
+        {/* Jumbo */}
+        <div className="jumbo" id="jumbo">
+          <h1>Kevin Jolley</h1>
+          <h2>JavaScript Web Developer</h2>
+
+          {/* Jumbo Nav */}
+          <div className="jumbo_nav">
+            <a href="#about">
+              {' '}
+              <p>About</p>{' '}
+            </a>
+            <a href="#skills">
+              {' '}
+              <p>Skills</p>{' '}
+            </a>
+            <p>Projects</p>
+            <p>Extra</p>
+          </div>
+        </div>
       </div>
     );
   }
