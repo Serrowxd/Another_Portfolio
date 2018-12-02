15d2be1c1f39d94e05d65356249aa90417cff464
Kevin Jolley <serrowpls@gmail.com>
2018-12-01 20:37:09 -0600

    updated deployment here

--- React/src/components/About.js
+++ React/src/components/About.js
@@ -3,7 +3,9 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>Henlo (OvO")</h1>
+      <div className="temporary">
+        <h1>Henlo (OvO") I display nothing about me!</h1>
+      </div>
     </Fragment>
   );
 };
