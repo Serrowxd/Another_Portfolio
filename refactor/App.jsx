2c60507354894a8986c29159b45c6c9e156a3045
Kevin Jolley <serrowpls@gmail.com>
2020-09-17 14:57:28 -0500

    working refactor, added react router

---
+++ refactor/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <Layout>
+      <h1>Henlo</h1>
+
+      {/* Switch / Router */}
+
+      {/* Loading Logic */}
+    </Layout>
+  );
+};
+
+export default App;
